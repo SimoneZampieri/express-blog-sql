7850fdc67566181d3662f953a33ccceff32bd0b5
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-05 16:12:27 +0100

    update] implement routing for post CRUD operations

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const router = express.Router();
+
+//controller
+const postController = require("../controllers/postController");
+
+//ROTTE
+
+router.get("/", postController.index);
+
+router.get("/id", postController.show);
+
+router.post("/", postController.store);
+
+router.put("/:id", postController.update);
+
+router.delete("/:id", postController.destroy);
+
+module.exports = router;
